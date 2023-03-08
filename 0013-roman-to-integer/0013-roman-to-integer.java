5e695e5c043ec58d1fb309a2ed6ed79513186506
suryansh-bhatnagar <90460257+suryansh-bhatnagar@users.noreply.github.com>
2023-03-08 22:42:49 +0530

    Time: 4 ms (82.70%), Space: 42.3 MB (80.39%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,78 +1,26 @@
 class Solution {
-    public int romanToInt(String s) {
-        
-        int res = 0;
-        char []temp = s.toCharArray();
-        for (int i = 0; i < s.toCharArray().length; i++) {
+public int romanToInt(String s) {
+    
+    int answer = 0, number = 0, prev = 0;
 
-            if(temp[i]=='I'){
-                if(i+1< s.toCharArray().length){
-                    if(temp[i+1]=='V'){
-                        res = res+4;
-                        i++;
-                    }
-                  else  if(temp[i+1]=='X'){
-                        res = res+9;
-                        i++;
-                    }
-                  else{
-                      res  =res+1;
-                    }
-                } else{
-                    res  =res+1;
-                }
-
-            }
-         else if(temp[i]=='X'){
-                if(i+1< s.toCharArray().length){
-                    if(temp[i+1]=='L'){
-                        res = res+40;
-                        i++;
-                    }
-                   else if(temp[i+1]=='C'){
-                        res = res+90;
-                        i++;
-                    }
-                   else{
-                       res= res+10;
-                    }
-                } else{
-                    res= res+10;
-                }
-
-            }
-           else if(temp[i]=='C'){
-                if(i+1< s.toCharArray().length){
-                    if(temp[i+1]=='D'){
-                        res = res+400;
-                        i++;
-                    }
-                   else if(temp[i+1]=='M'){
-                        res = res+900;
-                        i++;
-                    }else{
-                       res = res+100;
-                    }
-                }else{
-                    res = res+100;
-                }
-
-            }
-           else if(temp[i]=='V'){
-                res=res+5;
-            }
-           else if(temp[i]=='L'){
-                res=res+50;
-            }
-           else if(temp[i]=='D'){
-                res=res+500;
-            }
-            else if(temp[i]=='M'){
-                res=res+1000;
-            };
-            
+    for (int j = s.length() - 1; j >= 0; j--) {
+        switch (s.charAt(j)) {
+            case 'M' -> number = 1000;
+            case 'D' -> number = 500;
+            case 'C' -> number = 100;
+            case 'L' -> number = 50;
+            case 'X' -> number = 10;
+            case 'V' -> number = 5;
+            case 'I' -> number = 1;
         }
-        return res;
-
+        if (number < prev) {
+            answer -= number;
+        }
+        else {
+            answer += number;
+        }
+        prev = number;
     }
+    return answer;
+}
 }
